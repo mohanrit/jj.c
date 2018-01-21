8c683a4a447137a02ce1e056e5f73833b34336da
mohanrit <35220535+mohanrit@users.noreply.github.com>
2018-01-21 14:08:53 +0530

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+ int a,b,c,result = 0;
+printf("Enter a three digit integer: ");
+    scanf("%d", &a);
+b=a; while (b!= 0)
+    {
+     c= b%10;
+     result += c*c*c;
+     b/= 10;
+    }
+ if(result==a)
+        printf("yes");
+    else
+        printf("no");
+    return 0;
+    }
